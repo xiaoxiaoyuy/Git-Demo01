bd289cca10e7cf94801b017f65ca744bd2ae7129
xiaoyu <1076202404@qq.com>
2020-07-26 18:18:17 +0800

    修改了user.java的文件内容！

---
+++ user.java
@@ -0,0 +1,8 @@
+public class User(){
+      private int uid;
+      private String username;
+      private String password;
+      public void main(String[] args){
+          system.out.println(username);
+      }
+}
