6e0a2645a9732f72c892d58b3c2e25680038172b
xiaoyu <1076202404@qq.com>
2020-07-27 20:22:11 +0800

    为test.java文件添加了type属性！

--- test.java
+++ test.java
@@ -1,6 +1,7 @@
 public void Test(){
 	private String username;
 	private String password;
+	private String type;
 	public void main(String[] args){
 	      system.out.println(username,password);
 	}
