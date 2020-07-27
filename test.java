748770b78d1d5625a726dc0c5a8c490f5ee5caca
xiaoyu <1076202404@qq.com>
2020-07-27 20:20:10 +0800

    test.java多输出了password属性！

--- test.java
+++ test.java
@@ -2,5 +2,5 @@ public void Test(){
 	private String username;
 	private String password;
 	public void main(String[] args){
-	      system.out.println(username);
+	      system.out.println(username,password);
 	}
